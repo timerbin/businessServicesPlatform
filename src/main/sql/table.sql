ca5254bbc6b23e7be69b5d0e9d449d2fd110cf0d
wangwenbin <wangwenbin8023@sina.com.cn>
2016-11-28 18:17:52 +0800

    sql脚本提交

--- src/main/sql/table.sql
+++ src/main/sql/table.sql
@@ -1,6 +1,7 @@
 
+-- 创建数据库 --
+CREATE DATABASE platform DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
-
-
+ssh-keygen -t rsa -C "wangwenbin8023@sina.com" -f "C:\Users\Administrator\.ssh\id_rsa_plstform"
 
