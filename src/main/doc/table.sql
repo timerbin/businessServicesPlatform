e8a2d4e8f43c6498c75b78d7af5367c41b7e69ba
wangwenbin <wangwenbin8023@sina.com.cn>
2016-11-29 09:58:40 +0800

    添加生成mybatis 的mapper文件内容信息

--- src/main/doc/table.sql
+++ src/main/doc/table.sql
@@ -5,3 +5,5 @@ CREATE DATABASE platform DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 ssh-keygen -t rsa -C "wangwenbin8023@sina.com" -f "C:\Users\Administrator\.ssh\id_rsa_plstform"
 
+
+mybatis-generator:generate
