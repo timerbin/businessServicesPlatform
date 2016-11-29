3c345aafe693d36fb91afa4a80a68619b55f9377
wangwenbin <wangwenbin8023@sina.com.cn>
2016-11-29 10:06:50 +0800

    数据库信息更新

--- src/main/doc/table.sql
+++ src/main/doc/table.sql
@@ -1,9 +1,17 @@
 
 -- 创建数据库 --
 CREATE DATABASE platform DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- 创建用户--
+CREATE USER 'platform'@'localhost' IDENTIFIED BY 'platform'; 
+-- 数据库用户授权 --
+GRANT ALL ON platform.* TO 'platform'@'localhost'; 
 
 
 ssh-keygen -t rsa -C "wangwenbin8023@sina.com" -f "C:\Users\Administrator\.ssh\id_rsa_plstform"
 
 
 mybatis-generator:generate
+
+
+
+
