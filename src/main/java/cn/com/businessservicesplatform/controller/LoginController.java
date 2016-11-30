0f0b5b00661e36aa7ad5afeffe892abaafda3be8
wangwenbin <wangwenbin8023@sina.com>
2016-11-30 21:22:23 +0800

    登录代码提交

--- src/main/java/cn/com/businessservicesplatform/controller/LoginController.java
+++ src/main/java/cn/com/businessservicesplatform/controller/LoginController.java
@@ -1,5 +1,6 @@
 package cn.com.businessservicesplatform.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,39 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/login")
 public class LoginController {
 
-    @RequestMapping("/index")
-    @ResponseBody
-    public String index(HttpServletRequest request) {
-    	return  "1234";
-    }
-    
-    @RequestMapping("/toIndex")
-    public ModelAndView    toIndex(HttpServletRequest request) {
+	/**
+     * @Description: 跳转到登录页面 <br>
+     * @Author: wangwenbin <br>
+     * @Date: 2016年11月30日 <br>
+     * @Time: 下午7:59:17 <br>
+     * @return
+     * @return ModelAndView <br>
+     * @throws
+     */
+    @RequestMapping("/toLogin")
+    public ModelAndView toLogin(String callbackUrl) {
+    	ModelAndView model = new ModelAndView ( "/login/register");
+    	if(!StringUtils.isBlank(callbackUrl)){
+    		model.addObject("callbackUrl", callbackUrl);
+    	}else{
+    		model.addObject("callbackUrl", "");
+    	}
     	
-    	return new ModelAndView ( "index");
+    	return model;
+    }
+    /**
+     * @Description: 跳转到注册页面 <br>
+     * @Author: wangwenbin <br>
+     * @Date: 2016年11月30日 <br>
+     * @Time: 下午7:59:17 <br>
+     * @return
+     * @return ModelAndView <br>
+     * @throws
+     */
+    @RequestMapping("/toRegister")
+    public ModelAndView toRegister() {
+    	ModelAndView model = new ModelAndView ( "/login/register"); 
+    	
+    	return model;
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/login/register.jsp
@@ -0,0 +1,31 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>注册</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="renderer" content="webkit" />
+</head>
+<body>
+	<div >
+		<form action="#" method="post">
+			<dl>
+				<dt>用户名</dt>
+				<dd>
+					<input type="text" class="username"  placeholder="用户名/手机号" name="username" />
+				</dd>
+			</dl>
+			<dl>
+				<dt>密码</dt>
+				<dd>
+					<input type="password" class="password" name="password" autocomplete="off" />
+				</dd>
+			</dl>
+              <div>
+                  <a class="btn btn-red btn-full fn-mt15">提交</a>
+              </div>
+		</form>
+	</div>
+</body>
+</html>
