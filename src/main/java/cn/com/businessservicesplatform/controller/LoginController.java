994665a6df60059caf1a0f1c0e3d229b91b7279e
John <wangwenbin8023@sina.com>
2016-12-01 00:22:42 +0800

    添加验证码功能

--- src/main/java/cn/com/businessservicesplatform/controller/LoginController.java
+++ src/main/java/cn/com/businessservicesplatform/controller/LoginController.java
@@ -1,5 +1,6 @@
 package cn.com.businessservicesplatform.controller;
 
+import cn.com.businessservicesplatform.common.util.VerifyCodeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
 
 	/**
      * @Description: 跳转到登录页面 <br>
@@ -48,4 +53,39 @@ public class LoginController {
     	
     	return model;
     }
+
+
+	/**
+	 * 验证码
+	 * @Author John
+	 * @param request
+	 * @param response
+     * @return
+     */
+	@RequestMapping("/genVerifyCode")
+	@ResponseBody
+	public String genVerifyCode(HttpServletRequest request,HttpServletResponse response) {
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+		response.setContentType("image/jpeg");
+
+		//生成随机字串
+		String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+		//存入会话session
+		HttpSession session = request.getSession(true);
+		//删除以前的
+		session.removeAttribute("verCode");
+		session.setAttribute("verCode", verifyCode.toLowerCase());
+		//生成图片
+		int w = 100, h = 30;
+		try {
+			VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+		} catch (IOException e) {
+			//处理异常
+			e.printStackTrace();
+		}
+
+		return  verifyCode;
+	}
 }
--- src/main/webapp/WEB-INF/jsp/login/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login/login.jsp
@@ -4,6 +4,22 @@
     <title>登录</title>
 </head>
 <body>
-
+<table>
+    <tr>
+        <td nowrap width="437"></td>
+        <td>
+            <img id="img" src="${ctx}/authImage" />
+            <a href='#' onclick="javascript:changeImg()" style="color:white;"><label style="color:white;">看不清？</label></a>
+        </td>
+    </tr>
+</table>
 </body>
 </html>
+
+<!-- 触发JS刷新-->
+<script type="text/javascript">
+    function changeImg(){
+        var img = document.getElementById("img");
+        img.src = "${ctx}/authImage?date=" + new Date();;
+    }
+</script>
