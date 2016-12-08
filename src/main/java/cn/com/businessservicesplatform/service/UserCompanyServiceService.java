6e1a4db27ed46e688f6325a71baa8c198d7e3118
John <wangwenbin8023@sina.com>
2016-12-08 20:14:03 +0800

    服务管理201612082013

--- src/main/java/cn/com/businessservicesplatform/service/UserCompanyServiceService.java
+++ src/main/java/cn/com/businessservicesplatform/service/UserCompanyServiceService.java
@@ -3,6 +3,7 @@ package cn.com.businessservicesplatform.service;
 import java.util.List;
 
 import cn.com.businessservicesplatform.common.util.BasePage;
+import cn.com.businessservicesplatform.model.mysql.UserCompanyService;
 import cn.com.businessservicesplatform.model.vo.UserCompanyServiceVo;
 
 
@@ -17,7 +18,7 @@ public interface UserCompanyServiceService {
    
    public UserCompanyServiceVo getAllService(Integer id);
 
-   List<UserCompanyServiceVo> getAllUserCompanyServices(UserCompanyServiceVo vo);
+   List<UserCompanyService> getAllUserCompanyServices(UserCompanyServiceVo vo);
 
    public List<UserCompanyServiceVo> queryPage(BasePage basePage, UserCompanyServiceVo vo);
 
